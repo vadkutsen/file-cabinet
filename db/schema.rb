c0fc0591dd5bce27c3ba2e49b3abb9659ae946ab
vadkutsen <vadkutsen@gmail.com>
2018-06-20 04:06:52 +0200

    Create assocciation between users and docs

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_014250) do
+ActiveRecord::Schema.define(version: 2018_06_20_015123) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
